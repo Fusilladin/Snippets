de52b2f1d5a33a54345b8cae79b2cbe40a71e210
Fusilladin <91526142+Fusilladin@users.noreply.github.com>
2021-09-29 18:50:21 -0500

    Add files via upload

---
+++ Snippets.py
@@ -0,0 +1,32 @@
+# Snippets.txt
+
+# Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is
+
+# is:
+# is checks id:
+# id:               print(id(a))
+
+# Boolean Operations:
+# and
+# or
+# not
+
+# False Value:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. i.e., "", (), [].
+    # Any empty mapping. i.e., {}.
+
+Condition = False
+
+if condition:
+    print("Evaluated to True")
+else:
